f1f3856bbd9c96cf22694e3665bf697c3e842bf5
Atul Chauhan <atul14ac@gmail.com>
2021-06-11 21:45:51 +0530

    Home Completed

---
+++ src/components/Content/index.js
@@ -0,0 +1,4 @@
+export { default as Certifications } from "./Certifications";
+export { default as Connect } from "./Connect";
+export { default as Home } from "./Home";
+export { default as Projects } from "./Projects";
